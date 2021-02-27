2059ebdf917ce62d5c568246f709e8694a65d5ad
wmf76 <57199431+wmf76@users.noreply.github.com>
2021-02-27 12:53:33 -0600

    Add files via upload
    An update to include the test for the  function age_when_goal_meet and the full functionality of retirement.py

--- Retirement/retirement.py
+++ Retirement/retirement.py
@@ -4,6 +4,13 @@ def savings_per_year(salary,percent_saved):
 def num_years_till_goal(savings_goal,savings_per_year):
     return round(savings_goal/savings_per_year,0)
 
-def age_when_goal_met(age, num_years_till_goal):
-    return round(age+num_years_till_goal,0)
+def age_when_goal_met(age,salary,percent_saved,goal):
+    spy=savings_per_year(salary,percent_saved)
+    nyt=num_years_till_goal(goal,spy)
+    age_when_goal_meet= round(age+nyt,0)
+
+    goal_meet=True
+    if(age_when_goal_meet>=100):
+        goal_meet=False
+    return age_when_goal_meet, goal_meet
 
--- Retirement/retirement_test.py
+++ Retirement/retirement_test.py
@@ -6,6 +6,12 @@ class RetirementTest(unittest.TestCase):
     def test_savings_per_year(self):
         self.assertEqual(savings_per_year(65000,0.1),8775)
         self.assertEqual(savings_per_year(100000,0.15),20250)
+
     def test_years_till_goal(self):
         self.assertEqual(num_years_till_goal(1500000,8775),171)
         self.assertEqual(num_years_till_goal(500000,20250),25)
+
+    def test_age_when_goal_met(self):
+        self.assertEqual(age_when_goal_met(25,65000,0.1,1500000),(196.0,False))
+        self.assertEqual(age_when_goal_met(45,100000,0.15,500000),(70.0,True))
+        
